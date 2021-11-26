3307665b6eac5746d9a1893d753c5a92617057af
rmsmar <rms.marina89@gmail.com>
2021-11-26 14:42:28 +0300

    fixed test

--- src/test/java/ru/netology/OrderingCardDelivery.java
+++ src/test/java/ru/netology/OrderingCardDelivery.java
@@ -1,7 +1,9 @@
 package ru.netology;
 
-import com.codeborne.selenide.SelenideElement;
 import org.junit.jupiter.api.Test;
+
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 
 import static com.codeborne.selenide.Condition.*;
 import static com.codeborne.selenide.Selectors.withText;
@@ -9,32 +11,19 @@ import static com.codeborne.selenide.Selenide.*;
 
 public class OrderingCardDelivery {
 
+    LocalDate today = LocalDate.now();
+    LocalDate newDate = today.plusDays(3);
+    DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd.MM.yyyy");
+
     @Test
     void shouldRequestForCard() {
         open("http://localhost:9999");
-        SelenideElement form = $(".form");
-        form.$("[data-test-id=city] input").setValue("Пенза");
-        form.$("[data-test-id=date] input").setValue("22.11.2021");
-        form.$("[data-test-id=name] input").setValue("Иванов Иван");
-        form.$("[data-test-id=phone] input").setValue("+79200252879");
-        form.$("[data-test-id=agreement]").click();
-        form.$(".button").click();
-        $(withText("Успешно!")).waitUntil(exist, 15000);
-    }
-
-    @Test
-    void shouldSubmitRequestWithList() {
-        open("http://localhost:9999");
-        SelenideElement form = $(".form");
-        form.$("[data-test-id=city] input").setValue("Пе");
-        $$(".menu-item__control").last().click();
-        form.$("[data-test-id=date] input").setValue("");
-        form.$(".input__icon").click();
-        $$("td").find(exactText("10")).click();
-        form.$("[data-test-id=name] input").setValue("Иванов Иван");
-        form.$("[data-test-id=phone] input").setValue("+79200252879");
-        form.$("[data-test-id=agreement]").click();
-        form.$(".button").click();
+        $("[data-test-id=city] input").setValue("Пенза");
+        $("[data-test-id=date] input").doubleClick().sendKeys(formatter.format(newDate));
+        $("[data-test-id=name] input").setValue("Иванов Иван");
+        $("[data-test-id=phone] input").setValue("+79200252879");
+        $("[data-test-id=agreement]").click();
+        $(".button").click();
         $(withText("Успешно!")).waitUntil(exist, 15000);
     }
 }
