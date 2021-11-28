7cdd3f486c40fff4cb7039115769ce526e77483a
rmsmar <rms.marina89@gmail.com>
2021-11-28 12:55:15 +0300

    updated test

--- src/test/java/ru/netology/OrderingCardDelivery.java
+++ src/test/java/ru/netology/OrderingCardDelivery.java
@@ -1,7 +1,9 @@
 package ru.netology;
 
 import org.junit.jupiter.api.Test;
+import org.openqa.selenium.Keys;
 
+import java.time.Duration;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 
@@ -11,19 +13,23 @@ import static com.codeborne.selenide.Selenide.*;
 
 public class OrderingCardDelivery {
 
-    LocalDate today = LocalDate.now();
-    LocalDate newDate = today.plusDays(3);
-    DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd.MM.yyyy");
+    String generateDate(int days, String datePattern) {
+        return LocalDate.now().plusDays(days).format(DateTimeFormatter.ofPattern(datePattern));
+    }
 
     @Test
     void shouldRequestForCard() {
         open("http://localhost:9999");
         $("[data-test-id=city] input").setValue("Пенза");
-        $("[data-test-id=date] input").doubleClick().sendKeys(formatter.format(newDate));
+        $("[data-test-id=date] input").doubleClick().sendKeys(Keys.BACK_SPACE);
+        String planningDate = generateDate(4, "dd.MM.yyyy");
+        $("[data-test-id=date] input").setValue(planningDate);
         $("[data-test-id=name] input").setValue("Иванов Иван");
         $("[data-test-id=phone] input").setValue("+79200252879");
         $("[data-test-id=agreement]").click();
-        $(".button").click();
-        $(withText("Успешно!")).waitUntil(exist, 15000);
+        $$(".button").find(exactText("Забронировать")).click();
+        $(withText("Успешно")).shouldBe(visible, Duration.ofSeconds(15));
+        $(".notification__content").shouldBe(visible)
+                .shouldHave(exactText("Встреча успешно забронирована на " + planningDate));
     }
 }
